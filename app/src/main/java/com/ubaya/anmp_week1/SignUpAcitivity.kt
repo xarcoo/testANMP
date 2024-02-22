1bfe8375496ad57439e6fa1fcb578e71572e4fb1
abedneg00 <abedtitusnego@gmail.com>
2024-02-22 14:50:14 +0700

    comment signin added

--- app/src/main/java/com/ubaya/anmp_week1/SignUpAcitivity.kt
+++ app/src/main/java/com/ubaya/anmp_week1/SignUpAcitivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class SignUpAcitivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        //APA INI?
         //SIGN UP HIHIHIHA
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_acitivity)
