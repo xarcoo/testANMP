e9e38787d150191c94dda98fa1e919a9fdac4448
Yosiahans <s160421126@student.ubaya.ac.id>
2024-02-22 14:49:36 +0700

    TEST BRANCH
    yas

--- app/src/main/java/com/ubaya/anmp_week1/LoginActivity.kt
+++ app/src/main/java/com/ubaya/anmp_week1/LoginActivity.kt
@@ -9,9 +9,6 @@ class LoginActivity : AppCompatActivity() {
         //Cek Login ya
         //haloo frenn
         //cupu klean
-        //pppppppp
-
-        // TALIA
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
