627a1b525ba9ab35cb846b7bc33c4670c27baa4b
xarcoo <archieoematan21@gmail.com>
2024-02-22 14:27:55 +0700

    signup activity COMMENT

--- app/src/main/java/com/ubaya/anmp_week1/SignUpAcitivity.kt
+++ app/src/main/java/com/ubaya/anmp_week1/SignUpAcitivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class SignUpAcitivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //SIGN UP HIHIHIHA
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_acitivity)
     }
